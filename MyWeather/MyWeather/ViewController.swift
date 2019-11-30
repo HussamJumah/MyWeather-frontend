ccb799c6d59947cf2e9318a8ad92451032e75b02
Hussam Jumah <hussam.jum@gmail.com>
2019-11-29 21:29:40 -0500

    I created the xcode project.

---
+++ MyWeather/MyWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by Hussam Jumah on 11/29/19.
+//  Copyright © 2019 Hussam Jumah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
