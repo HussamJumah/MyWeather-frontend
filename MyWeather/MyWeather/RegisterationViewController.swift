97bda7b53d52aa9dbbbc7411704eedb37a98d48d
Hussam Jumah <hussam.jum@gmail.com>
2019-11-30 22:27:21 -0500

    Created Registration page and coded the cancel button to dismiss.

---
+++ MyWeather/MyWeather/RegisterationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RegisterationViewController.swift
+//  MyWeather
+//
+//  Created by Hussam Jumah on 11/30/19.
+//  Copyright © 2019 Hussam Jumah. All rights reserved.
+//
+
+import UIKit
+
+class RegisterationViewController: UIViewController {
+
+    @IBAction func CancelTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
